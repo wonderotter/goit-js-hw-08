fb1e4c2de247e2aec02e57d8db0c17f69bb54aa5
Kateryna Skepska <118077803+wonderotter@users.noreply.github.com>
2023-04-09 13:58:38 +0300

    fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+    localStorage.setItem(CURRENT_TIME_KEY, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
